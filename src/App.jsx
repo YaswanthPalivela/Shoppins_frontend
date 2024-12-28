208ae1b11527a27300a65fc72e3ca8f1b44c6175
yaswanthpalivela <yaswanthnani998@gmail.com>
2024-12-28 16:17:32 +0530

    homepage Finished

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Applestudio />} />
         <Route path="/watches" element={<AppleWatch />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/NotFound" element={<NotFound />} />
       </Routes>
     </Router>
   );
